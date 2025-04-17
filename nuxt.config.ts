05f9e1d5f5ec9c51116c49ee5e836954dbcaa5ae
victorandresp <ponceottamendi27@gmail.com>
2025-04-16 22:34:13 -0400

    feat: installation and config of apollo graphql :sparkles:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,27 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/apollo',
+  ],
+  runtimeConfig: {
+    public: {
+      // Variables access only on client side
+      frontendUrl: process.env.FRONTEND_URL, 
+    },
+    // Variables access only on server side
+    graphqlUrl: process.env.API_KEY, 
+  },
+  apollo: {
+    authType: "Bearer",
+    authHeader: "Authorization",
+    tokenStorage: "cookie",
+    clients: {
+      default: {
+        tokenName: "auth-token",
+        httpEndpoint: process.env.GRAPHQL_URL as string,
+      },
+    }
+  }
 })
