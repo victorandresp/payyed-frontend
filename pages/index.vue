00a61d16e21eca58c1f00eebc0dead1c31a03baf
victorandresp <ponceottamendi27@gmail.com>
2025-04-16 23:45:57 -0400

    feat: text first query for graphql :sparkles:

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,24 @@
 <template>
-    <div>Default Page</div>
+    <div>
+        <div>Default Page</div>
+        <p v-if="!loading">{{ result.getUser }}</p>
+    </div>
 </template>
+<script setup lang="ts">
+    const text = ref({})
+    const query = gql`
+        query{
+            getUser{
+                id,
+                name
+            }
+        }`
+
+      const { result, loading } = useQuery(query);
+
+      if (!loading) {
+        console.log(result);
+        
+      }
+
+</script>
