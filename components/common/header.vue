de13cbe51b729c1d4566c1563ae74c3c5fa85bbe
victorandresp <ponceottamendi27@gmail.com>
2025-04-23 23:11:16 -0400

    feat: header web :sparkles:

--- components/common/header.vue
+++ components/common/header.vue
@@ -1,15 +1,30 @@
-<script lang="ts">
+<script setup lang="ts">
+const menuItems = ref([
+    { name: 'Send'},
+    { name: 'Recieve'},
+    { name: 'About Us'},
+    { name: 'Fees'},
+    { name: 'Help'},
+])
 </script>
 
 <template>
     <v-app-bar>
-        <v-app-bar-title>
-            <v-img width="auto" src="/assets/images/payyed-logo.png"/>
-        </v-app-bar-title>
-        <v-app-bar-items>
-            <v-btn>hola</v-btn>
-            <v-btn>hola</v-btn>
-            <v-btn>hola</v-btn>
-        </v-app-bar-items>
+        <v-row>
+            <v-spacer/>
+            <v-col cols="auto">
+                <v-img width="auto" src="/assets/images/payyed-logo.png"/>
+            </v-col>
+            <v-col cols="6">
+                <template v-for="item in menuItems" :key="item.name">
+                    <v-btn text>{{ item.name }}</v-btn>
+                </template>
+            </v-col>
+            <v-col cols="2">
+                <v-btn>login</v-btn>
+                <v-btn color="primary" variant="flat">sign up</v-btn>
+            </v-col>
+            <v-spacer/>
+        </v-row>
     </v-app-bar>
 </template>
