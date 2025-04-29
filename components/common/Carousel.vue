b5c276463cdbcec809abb4ece020368ff6c2c690
victorandresp <ponceottamendi27@gmail.com>
2025-04-28 23:26:04 -0400

    feat: gradient to carousel and dynamic texts :sparkles:

--- components/common/Carousel.vue
+++ components/common/Carousel.vue
@@ -9,9 +9,20 @@ const { result: carouselImages, loading } = useQuery(GET_CAROUSEL_IMAGES)
    <!-- TODO: MAKE CALCULE FOR HEIGHT 100vh - 64px (default value for v-app-bar) -->
   <v-carousel v-if="!loading && carouselImages.getCarouselImages.length > 0" height="94vh" show-arrows="hover" hide-delimiters> 
     <template v-for="carousel in carouselImages.getCarouselImages" :key="carousel.path">
-      <v-carousel-item :src="carousel.path" cover />
+      <v-carousel-item  :src="carousel.path" cover>
+        <div class="h-100 d-flex justify-center align-center gradient-common-carousel">
+          <div :class="'w-50 '+`${carousel.orientation}`">
+            <h1 class="text-white text-h2 font-weight-bold pb-3">{{ carousel.title }}</h1>
+            <h3 class="text-white text-h5 font-weight-light">{{  carousel.subtitle }}</h3>
+          </div>
+        </div>
+      </v-carousel-item>
     </template>
   </v-carousel>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.gradient-common-carousel{
+  background: linear-gradient(45deg, #00000017, #0000007a);
+}
+</style>
