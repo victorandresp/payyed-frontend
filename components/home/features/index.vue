685ba649078ce65afd75d43700cb509dbae28ca3
victorandresp <ponceottamendi27@gmail.com>
2025-04-29 21:49:18 -0400

    feat(features): title and subtitle :sparkles:

--- components/home/features/index.vue
+++ components/home/features/index.vue
@@ -2,5 +2,13 @@
 </script>
 
 <template>
+    <v-container class="py-14">
+        <v-row>
+            <v-col class="text-center" cols="12">
+                <h2 class="text-h4 font-weight-bold">Why should you choose Payyed?</h2>
+                <p class="text-h6 font-weight-thin">Here’s Top 4 reasons why using a Payyed account for manage your money.</p>
+            </v-col>
+        </v-row>
+    </v-container>
   <HomeFeaturesCard/>
 </template>
