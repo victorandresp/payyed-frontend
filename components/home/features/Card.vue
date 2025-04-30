e0762e2e245a75c4e15f6a4573bc852b18a655b6
victorandresp <ponceottamendi27@gmail.com>
2025-04-29 22:42:01 -0400

    feat(features): cards:sparkles:

--- components/home/features/Card.vue
+++ components/home/features/Card.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
+defineProps({
+    feature: {
+    type: Object,
+    required: true,
+  },
+});
 </script>
 
 <template>
-  <div>
-    <h1>Carddddd</h1>
-  </div>
+    <v-card elevation="0">
+        <v-card-title primary-title>
+            <v-icon class="mb-3" :icon="feature.icon" color="primary"/>
+            <p class="font-weight-bold">{{ feature.title }}</p>
+        </v-card-title>
+        <v-card-text  primary-title>
+            {{ feature.description }}
+        </v-card-text>
+        <v-card-actions>
+            <v-btn color="primary" append-icon="mdi-chevron-right" >
+                Learn more
+            </v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
--- components/home/features/index.vue
+++ components/home/features/index.vue
@@ -1,4 +1,30 @@
 <script setup lang="ts">
+const features = [
+    {
+        icon: 'mdi-hand',
+        title: 'Easy to use',
+        description: 'Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.',
+        link: '/easy-to-use'
+    },
+    {
+        icon: 'mdi-arrow-top-right',
+        title: 'Faster Payments',
+        description: 'Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.',
+        link: '/easy-to-use'
+    },
+    {
+        icon: 'mdi-currency-usd',
+        title: 'Lower Fees',
+        description: 'Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.',
+        link: '/easy-to-use'
+    },
+    {
+        icon: 'mdi-lock',
+        title: '100% secure',
+        description: 'Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.',
+        link: '/easy-to-use'
+    },
+]
 </script>
 
 <template>
@@ -9,6 +35,18 @@
                 <p class="text-h6 font-weight-thin">Here’s Top 4 reasons why using a Payyed account for manage your money.</p>
             </v-col>
         </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-row justify="center">
+                    <template v-for="feature in features" :key="feature.link">
+                        <v-col cols="2">
+                            <HomeFeaturesCard :feature="feature"/>
+                        </v-col>
+                    </template>
+                </v-row>
+            </v-col>
+        </v-row>
     </v-container>
-  <HomeFeaturesCard/>
+    
+  
 </template>
