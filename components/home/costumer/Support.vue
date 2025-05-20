0586741f77328279af4fe6515daa8945032b013f
victorandresp <ponceottamendi27@gmail.com>
2025-05-19 21:53:08 -0400

    feat(testimony): added gradient to parallax :sparkles:

--- components/home/costumer/Support.vue
+++ components/home/costumer/Support.vue
@@ -4,7 +4,17 @@
 </script>
 
 <template>
-    <v-parallax scale="0.1" height="600px" src="/public/parallax/image-home-parallax.jpg" >
-      <div>Test</div>
+    <v-parallax class="gradient-customer-support" scale="0.1" height="600px" src="/public/parallax/image-home-parallax.jpg" >
+      <div class="d-flex w-100 h-100 gradient-customer-support">
+        <div class="text-white">
+          Hola
+        </div>
+      </div>
     </v-parallax>
 </template>
+
+<style lang="scss">
+.gradient-customer-support{
+  background: linear-gradient(45deg, #00000073, #000000d9);
+}
+</style>
