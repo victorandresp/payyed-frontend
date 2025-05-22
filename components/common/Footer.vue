92d54b7be29e11bb8fe3b86056544236c6901db8
victorandresp <ponceottamendi27@gmail.com>
2025-05-21 21:18:29 -0400

    feat(footer): links :sparkles:

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -1,10 +1,27 @@
 <script lang="ts" setup>
-console.log('calledddddddddddddddd');
+const links = [
+  { route: '/about-us', name: 'About Us'},
+  { route: '/support', name: 'Support'},
+  { route: '/help', name: 'Help'},
+  { route: '/careers', name: 'Affiliate'},
+  { route: '/fees', name: 'Fees'},
+]
 
 </script>
 <template>
- <v-footer>
-    <h1> Hola</h1>
+ <v-footer class="pa-10">
+  <v-row justify="space-between">
+    <v-col>
+      <div class="d-flex">
+        <template v-for="link in links" :key="link.route">
+          <NuxtLink class="text-black text-decoration-none ml-4" :to="link.route">
+            {{ link.name }}
+          </NuxtLink>
+
+        </template>
+      </div>
+    </v-col>
+  </v-row>
  </v-footer>
 </template>
 <style></style>
