df1cdca0abbd8308d1164b69cd3b8c1bb4b2cf73
victorandresp <ponceottamendi27@gmail.com>
2025-05-21 20:38:41 -0400

    feat(market): section for market app :sparkles:

---
+++ components/home/market/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <div class="w-100 bg-blue-grey-lighten-4 text-center pa-5">
+    <h4 class="text-h4 font-weight-bold mb-2">Get the app</h4>
+    <p class="text-subtitle-1">Download our app for the fastest, most convenient way to send & get Payment.</p>
+    <div class="d-inline-flex ga-6 mt-4">
+      <v-img width="150" src="/assets/images/app-store.png" />
+      <v-img width="150" src="/assets/images/google-play-store.png" />
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
         <HomeFeatures />
         <HomeCostumerTestimony />
         <HomeCostumerSupport />
-        <v-sheet class="mt-7 w-100 h-30 pa-12" color="grey-lighten-3" />
+        <HomeMarket/>
     </v-container>
 </template>
 <script setup lang="ts">
