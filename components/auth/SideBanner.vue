c25a6d96cd071f3673f3ee7c445a801075e39b19
victorandresp <ponceottamendi27@gmail.com>
2025-06-03 22:04:17 -0400

    feat(login): background gradient :sparkles:

--- components/auth/SideBanner.vue
+++ components/auth/SideBanner.vue
@@ -3,11 +3,16 @@
 <template>
     <div class="w-100 h-100">
         <div class="login-background-image">
-            <p>Hola</p>
+            <div class="h-100 bg-primary opacity-80">
+                <p>Hola</p>
+            </div>
         </div>
     </div>
 </template>
 <style lang="scss">
+.login-background-gradient{
+    background-color: #2dbe60;
+}
 .login-background-image{
     width: 100%;
     height: 100vh;
