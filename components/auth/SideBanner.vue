34c72765d1f8caaacb0d9d25b656f34ad22b82c4
victorandresp <ponceottamendi27@gmail.com>
2025-06-03 23:14:49 -0400

    feat(login): side bar desing :sparkles:

--- components/auth/SideBanner.vue
+++ components/auth/SideBanner.vue
@@ -4,8 +4,8 @@
     <div class="w-100">
         <div class="login-background-image">
             <div class="h-screen bg-primary opacity-80">
-                <img width="auto" src="/assets/images/payyed-logo-white.png" >
-                <div class="w-100 h-100 d-flex flex-column justify-center align-center">
+                <img width="auto" class="pa-10" src="/assets/images/payyed-logo-white.png" >
+                <div class="w-100 h-75 px-10 d-flex flex-column justify-center align-center">
                     <h1 class="text-white text-h3 font-weight-bold pb-3 w-100">Welcome back!</h1>
                     <h3 class="text-white text-h6 font-weight-light w-100">We are glad to see you again! Instant deposits, withdrawals & payouts trusted by millions worldwide.</h3>
                 </div>
