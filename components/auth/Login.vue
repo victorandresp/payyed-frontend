48a77cb26850173f710fe874a1cd830961a8df64
victorandresp <ponceottamendi27@gmail.com>
2025-06-09 20:14:46 -0400

    feat(login): login design :sparkles:

--- components/auth/Login.vue
+++ components/auth/Login.vue
@@ -2,19 +2,26 @@
 
 </script>
 <template>
-  <div class="d-flex w-75 h-100 justify-center align-center">
-    <v-row class="mx-auto" justify="center">
-      <v-col>
-        <h3 class="text-h5 font-weight-light w-100">Login</h3>
+  <div class="d-flex w-100 h-100 justify-center align-center">
+      <div class="w-75">
+        <h3 class="text-h5 font-weight-light w-100 pb-2">Log In</h3>
         <p>Email</p>
         <v-text-field
+            type="email"
             density="compact"
             prepend-inner-icon="mdi-email-outline"
             variant="outlined"
           />
-      </v-col>
-
-    </v-row>
+        <p>Password</p>
+        <v-text-field
+            type="password"
+            density="compact"
+            prepend-inner-icon="mdi-lock-outline"
+            variant="outlined"
+          />
+          <v-btn width="100%" variant="outlined" color="white">Login</v-btn>
+          <p class="mt-3 text-right">Don't have an account? <NuxtLink class="text-white text-weight-bold" to="/auth/sign-up">Sign Up</NuxtLink></p>
+      </div>
   </div>
 </template>
 
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -6,13 +6,13 @@ definePageMeta({
 <template>
     <div>
       <v-row>
-        <v-col cols="6" class="pb-0">
+        <v-col cols="6" class="pb-0 pr-0 bg-primary">
           <AuthSideBanner 
            title="Welcome back!" 
            subtitle="We are glad to see you again! Instant deposits, withdrawals & payouts trusted by millions worldwide." 
            />
         </v-col>
-        <v-col cols="6" class="pb-0">
+        <v-col cols="6" class="pb-0 px-0 bg-primary">
           <AuthLogin/>
         </v-col>
       </v-row>
