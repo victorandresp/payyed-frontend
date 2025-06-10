da8acbcce16896285eb541975285dbaf3690ca78
victorandresp <ponceottamendi27@gmail.com>
2025-06-09 20:38:10 -0400

    feat(SignUp): design :sparkles:

---
+++ components/auth/SignUp.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <div class="d-flex w-100 h-100 justify-center align-center">
+      <div class="w-75">
+        <h3 class="text-h5 font-weight-light w-100 pb-2">Sign Up</h3>
+        <p>Name</p>
+        <v-text-field
+            type="text"
+            density="compact"
+            prepend-inner-icon="mdi-account-outline"
+            variant="outlined"
+          />
+        <p>Last Name</p>
+        <v-text-field
+            type="text"
+            density="compact"
+            prepend-inner-icon="mdi-account-outline"
+            variant="outlined"
+          />
+        <p>Email</p>
+        <v-text-field
+            type="email"
+            density="compact"
+            prepend-inner-icon="mdi-email-outline"
+            variant="outlined"
+          />
+        <p>Password</p>
+        <v-text-field
+            type="password"
+            density="compact"
+            prepend-inner-icon="mdi-lock-outline"
+            variant="outlined"
+          />
+        <p>Repeat Password</p>
+        <v-text-field
+            type="password"
+            density="compact"
+            prepend-inner-icon="mdi-lock-outline"
+            variant="outlined"
+          />
+          <v-btn width="100%" variant="outlined" color="white">Login</v-btn>
+          <p class="mt-3 text-right">Already have an account? <NuxtLink class="text-white text-weight-bold" to="/auth/login">Log In</NuxtLink></p>
+      </div>
+  </div>
+</template>
+
+
+<style>
+
+</style>
---
+++ pages/auth/sign-up.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'auth'
+})
+</script>
+<template>
+    <div>
+      <v-row>
+        <v-col cols="6" class="pb-0 pr-0 bg-primary">
+          <AuthSideBanner 
+           title="Get Verified!" 
+           subtitle="Every day, Payyed makes thousands of customers happy." 
+           />
+        </v-col>
+        <v-col cols="6" class="pb-0 px-0 bg-primary">
+          <AuthSignUp/>
+        </v-col>
+      </v-row>
+    </div>
+</template>
+<style lang="scss">
+</style>
