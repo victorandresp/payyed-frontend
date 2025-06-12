e2ab756dd14d1fa1e900d8f351d3b8a8668b643d
victorandresp <ponceottamendi27@gmail.com>
2025-06-11 21:56:08 -0400

    feat(login): visible eye password field :sparkles:

--- components/auth/Login.vue
+++ components/auth/Login.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-
+ const isVisiblePassword = ref(false)
 </script>
 <template>
   <div class="d-flex w-100 h-100 justify-center align-center">
@@ -14,10 +14,12 @@
           />
         <p>Password</p>
         <v-text-field
-            type="password"
+            :append-inner-icon="isVisiblePassword ? 'mdi-eye-off' : 'mdi-eye'"
+            :type="isVisiblePassword ? 'text' : 'password'"
             density="compact"
             prepend-inner-icon="mdi-lock-outline"
             variant="outlined"
+            @click:append-inner="isVisiblePassword = !isVisiblePassword"
           />
           <v-btn width="100%" variant="outlined" color="white">Login</v-btn>
           <p class="mt-3 text-right">Don't have an account? <NuxtLink class="text-white text-weight-bold" to="/auth/sign-up">Sign Up</NuxtLink></p>
