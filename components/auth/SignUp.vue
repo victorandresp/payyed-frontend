b1b62fddef5ae66ad5e0f8b8da184c59ae2555e3
victorandresp <ponceottamendi27@gmail.com>
2025-06-11 21:58:26 -0400

    feat(SignUp): visible eye password field :sparkles:

--- components/auth/SignUp.vue
+++ components/auth/SignUp.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-
+const isVisiblePassword = ref(false)
+const isVisibleRepeatPassword = ref(false)
 </script>
 <template>
   <div class="d-flex w-100 h-100 justify-center align-center">
@@ -28,17 +29,21 @@
           />
         <p>Password</p>
         <v-text-field
-            type="password"
+            :append-inner-icon="isVisiblePassword ? 'mdi-eye-off' : 'mdi-eye'"
+            :type="isVisiblePassword ? 'text' : 'password'"
             density="compact"
             prepend-inner-icon="mdi-lock-outline"
             variant="outlined"
+            @click:append-inner="isVisiblePassword = !isVisiblePassword"
           />
         <p>Repeat Password</p>
         <v-text-field
-            type="password"
+            :append-inner-icon="isVisibleRepeatPassword ? 'mdi-eye-off' : 'mdi-eye'"
+            :type="isVisibleRepeatPassword ? 'text' : 'password'"
             density="compact"
             prepend-inner-icon="mdi-lock-outline"
             variant="outlined"
+            @click:append-inner="isVisibleRepeatPassword = !isVisibleRepeatPassword"
           />
           <v-btn width="100%" variant="outlined" color="white">Login</v-btn>
           <p class="mt-3 text-right">Already have an account? <NuxtLink class="text-white text-weight-bold" to="/auth/login">Log In</NuxtLink></p>
